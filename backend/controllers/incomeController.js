7bd11144ed577987a87079ca4b0e94c9f17a7efe
Mudit Kumar Singh <muditksingh96184@gmail.com>
2026-03-29 09:49:34 +0530

    Final msg

---
+++ backend/controllers/incomeController.js
@@ -0,0 +1,95 @@
+const User = require('../models/User')
+const Income = require('../models/Income')
+const xlsx = require('xlsx')
+
+const addIncome = async (req, res) => {
+    const userId = req.user.id;
+
+    try {
+        const { icon, source, amount, date } = req.body;
+
+        //validate the mission fields
+        if (!source || !amount || !date) {
+            return res.status(400).json({ message: "All fields are required" })
+        }
+
+        const newIncome = new Income({
+            userId,
+            icon,
+            source,
+            amount,
+            date: new Date(date)
+        })
+
+        await newIncome.save();
+        res.status(200).json(newIncome);
+
+    } catch (err) {
+        res.status(500).json({ message: "Server Error" })
+    }
+}
+
+const getAllIncome = async (req, res) => {
+    const userId = req.user.id
+    try {
+        const income = await Income.find({ userId }).sort({ date: -1 })
+        res.json(income)
+    } catch (err) {
+        res.status(500).json({ message: "Server Error" })
+    }
+}
+
+const deleteIncome = async (req, res) => {
+    try {
+        await Income.findByIdAndDelete(req.params.id);
+        res.json({ message: "Income Deleted Successfully" })
+    } catch (err) {
+        res.status(500).json({ message: "Server Error" })
+    }
+}
+
+const downloadIncomeExcel = async (req, res) => {
+    const userId = req.user.id;
+
+    try {
+        const income = await Income.find({ userId }).sort({ date: -1 });
+
+        const data = income.map((item) => ({
+            Source: item.source,
+            Amount: item.amount,
+            Date: item.date,
+        }));
+
+        const wb = xlsx.utils.book_new();
+        const ws = xlsx.utils.json_to_sheet(data);
+        xlsx.utils.book_append_sheet(wb, ws, "Income");
+
+        // create buffer instead of file
+        const buffer = xlsx.write(wb, {
+            type: "buffer",
+            bookType: "xlsx",
+        });
+
+        res.setHeader(
+            "Content-Type",
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        );
+
+        res.setHeader(
+            "Content-Disposition",
+            "attachment; filename=income_details.xlsx"
+        );
+
+        res.send(buffer);
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+module.exports = {
+    addIncome,
+    getAllIncome,
+    downloadIncomeExcel,
+    deleteIncome
+}
